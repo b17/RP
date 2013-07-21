2b8cb62e985d6b3726ac6024b1ad955ce6e09442
nazymko <nazymko.patronus@gmail.com>
2013-07-21 23:35:48 +0300

    announce

---
+++ app/models/announce.rb
@@ -0,0 +1,3 @@
+class Announce < ActiveRecord::Base
+  attr_accessible :date, :desc, :lg, :lt, :main_img_url, :title
+end
---
+++ db/migrate/20130721112356_create_announces.rb
@@ -0,0 +1,14 @@
+class CreateAnnounces < ActiveRecord::Migration
+  def change
+    create_table :announces do |t|
+      t.string :title
+      t.string :desc
+      t.float :lg
+      t.float :lt
+      t.string :main_img_url
+      t.datetime :event_date
+
+      t.timestamps
+    end
+  end
+end
