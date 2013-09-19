5018f74179d357e0cf9fbe44d1d9ca739e9f4b08
b17 <erelus@gmail.com>
2013-09-19 22:49:02 +0300

    Fix tests.

--- db/migrate/20130917171226_create_news_taggers.rb
+++ db/migrate/20130917171226_create_news_taggers.rb
@@ -1,12 +1,12 @@
 class CreateNewsTaggers < ActiveRecord::Migration
   def up
-    create_table :news_tagger, :id=>false do |t|
+    create_table :news_taggers, :id=>false do |t|
       t.integer :tag_id
       t.integer :news_id
     end
   end
 
   def down
-    drop_table :news_tagger
+    drop_table :news_taggers
   end
 end
