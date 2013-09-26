ae81184fe367550f261f06754f8b3efb8719090b
Andrew <nazymko.patronus@gmail.com>
2013-09-26 23:43:04 +0300

    search page, flexibility

--- app/controllers/announce/default_controller.rb
+++ app/controllers/announce/default_controller.rb
@@ -1,11 +1,13 @@
 class Announce::DefaultController < ApplicationController
 
-  layout 'search'
+  layout 'search' #same like guest , but with search.css
 
   def search
     where = layer_announce.bind_and_apply params
+    news_where = layer_news.bind_and_apply params
     @announces = layer_announce.query where
     @filters = layer_announce.filters params, request
+    @news =layer_news.query news_where
   end
 
   def view
