b21a288e55364521ac55e51b8cc50cbb457c2317
b17 <erelus@gmail.com>
2013-10-03 22:06:34 +0300

    Calendar controls.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,9 +45,82 @@
                 $this.data('geomap', data);
             } else {
                 if (typeof options == 'string') {
-                    return data[options].apply(this, args.slice(1));
+                    return data[options].apply(data, args.slice(1));
                 }
             }
         })
+    }
+})(jQuery);
+
+(function($) {
+    var CalendarControls = function(el) {
+        this.$el = $(el);
+        this.$prev = $(el).find('.js-prev');
+        this.$next = $(el).find('.js-next');
+        this.items = $(el).find('.js-item').addClass('hidden');
+
+        this.current = 0;
+        this.slideTo(0);
+        this.bind();
+    };
+
+    CalendarControls.prototype.bind = function() {
+        var self = this;
+        this.$prev.click(function(e) {
+            e.preventDefault();
+            self.prev();
+        });
+
+        this.$next.click(function(e) {
+            e.preventDefault();
+            self.next();
+        });
+    };
+
+    CalendarControls.prototype.slideTo = function(number) {
+        if (number >= 0 && number < this.items.length) {
+            var $current = $(this.items[this.current]);
+            $current.addClass('hidden');
+            var $target = $(this.items[number]);
+            $target.removeClass('hidden');
+
+            this.current = number;
+
+            if (this.current >= (this.items.length - 1)) {
+                this.$next.addClass('hidden');
+            } else {
+                this.$next.removeClass('hidden');
+            }
+
+            if (this.current > 0) {
+                this.$prev.removeClass('hidden');
+            } else {
+                this.$prev.addClass('hidden');
+            }
+        }
+    }
+
+    CalendarControls.prototype.next = function() {
+        this.slideTo(this.current + 1);
+    };
+
+    CalendarControls.prototype.prev = function() {
+        this.slideTo(this.current - 1);
+    };
+
+    $.fn.calendarControls = function(options) {
+        var args = arguments;
+        return $(this).each(function() {
+            var data = $(this).data('calendar_controls');
+
+            if (!data) {
+                data = new CalendarControls(this);
+                $(this).data('calendar_controls', data);
+            } else {
+                if (typeof options === 'string') {
+                    return data[options].apply(data, args.slice(1));
+                }
+            }
+        });
     }
 })(jQuery);
